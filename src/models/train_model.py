1e04a8447abbda87ee76812bca9423e2ba805c26
elenamp28 <elenamp28@gmail.com>
2023-11-13 16:42:14 +0100

    update train_model

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -53,7 +53,7 @@ def train(config):
 
     metric = load_metric("accuracy")
     model = AutoModelForImageClassification.from_pretrained(
-        model_name_or_path,
+        pretrained_model,
         num_labels=len(labels),
         id2label={str(i): c for i, c in enumerate(labels)},
         label2id={c: str(i) for i, c in enumerate(labels)}
