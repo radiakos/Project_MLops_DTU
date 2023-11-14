5d50968a6541da5b5e52250b2e25972e11d5addd
Elena <elenamp28@gmail.com>
2023-11-14 10:55:20 +0100

    update project description

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='A short description of the project.',
+    description='Fruit image classification using Vision Transformer for the cours ML Ops at DTU.',
     author='Theodoros',
     license='',
 )
